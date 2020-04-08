9cecd8737912b17be71bedb267d080c30e8df6e9
detecti272 <61080189+detecti272@users.noreply.github.com>
2020-04-08 14:05:50 +0530

    Update autochangebio.py

--- stdplugins/autochangebio.py
+++ stdplugins/autochangebio.py
@@ -1,7 +1,8 @@
 # For @UniBorg
 # (c) Shrimadhav U K
 """Auto Profile Updation Commands
-.autopp"""
+.autoname
+.autobio"""
 import asyncio
 import time
 from telethon.tl import functions
@@ -19,7 +20,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} | Longer Bio: @Detec_tive| ⌚️ {HM}"
+        bio = f"📅 {DMY} | 😎Muttahir's Bot😎 | ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
@@ -32,6 +33,31 @@ async def _(event):
             # logger.info(r.stringify())
             # await borg.send_message(  # pylint:disable=E0602
             #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
-            #     "Changed Profile Picture"
+            #     "Successfully Changed Profile Bio"
+            # )
+        await asyncio.sleep(DEL_TIME_OUT)
+
+
+@borg.on(admin_cmd("autoname"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    while True:
+        DM = time.strftime("%d.%m.%y")
+        HM = time.strftime("%H:%M")
+        name = f"⌚{HM}|😎Muttahir😎|📅{DM}"
+        logger.info(name)
+        try:
+            await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                first_name=name
+            ))
+        except FloodWaitError as ex:
+            logger.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+        # else:
+            # logger.info(r.stringify())
+            # await borg.send_message(  # pylint:disable=E0602
+            #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+            #     "Successfully Changed Profile Name"
             # )
         await asyncio.sleep(DEL_TIME_OUT)
